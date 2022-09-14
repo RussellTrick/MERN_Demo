6748b76f68c17d630175c29af9c4d7b404f3d38c
Owain <owain.evans@hotmail.co.uk>
2022-09-14 17:04:31 +0100

    Refactored error messages and made signin api

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,12 +1,25 @@
-const UserModel = require('../models/Users');
+const UserModel = require("../models/Users");
 const bcrypt = require("bcrypt");
+const { sendError } = require("../utils/helper");
 
 exports.createUser = async (req, res) => {
-    const body = req.body;
-    
-    const user = new UserModel(body);
-    const salt = await bcrypt.genSalt(10);
-    
-    user.password = await bcrypt.hash(user.password, salt);
-    user.save().then((doc) => res.status(201).send(doc));
-}
+  const body = req.body;
+  const exists = await UserModel.findOne({ email: body.email });
+  if (exists) return sendError(res, "This email already exists.");
+
+  const user = new UserModel(body);
+
+  await user.save().then((doc) => res.status(201).send(doc));
+};
+
+exports.signIn = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email.trim() || !password.trim())
+    return sendError(res, "Missing email/password.");
+
+  const user = await UserModel.findOne({ email });
+  if (!user) return sendError(res, "User not found.");
+
+  const isMatched = await user.comparePasswords(password);
+  if (!isMatched) return sendError(res, "Passwords do not match.");
+};
--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,25 +1,38 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    type: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+  }
+  next();
+});
+
+UserSchema.methods.comparePasswords = async function (password) {
+  const result = await bcrypt.compareSync(password, this.password);
+  return result;
+};
 
 const UserModel = mongoose.model("users", UserSchema);
 module.exports = UserModel;
