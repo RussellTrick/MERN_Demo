f10c3347c71d4531b9fbdfbd799b2dcd8a94fdb7
Owain <owain.evans@hotmail.co.uk>
2022-09-14 15:00:57 +0100

    dot env package

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app  = express();
 const mongoose = require('mongoose');
-const config = require('./config.json');
 const UserModel = require('./models/Users');
+const bcrypt = require("bcrypt");
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 8000
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(config.dbConnect)
+mongoose.connect(process.env.dbConnect)
 
 app.get("/getUsers", (req, res) => {
     UserModel.find({}, (err, result) => {
@@ -20,14 +24,32 @@ app.get("/getUsers", (req, res) => {
     })
 })
 
-app.post("/createUser", async (req, res) => {
-    const user = req.body
-    const newUser = new UserModel(user);
-    await newUser.save();
+app.post("/signup", async (req, res) => {
+    const body = req.body;
 
-    res.json(user);
-})
+    if (!(body.email)) {
+        return res.status(400).send({ error: "Data doesn't contain email or is formmated incorrectly" });
+    }
 
-app.listen(3001, () => {
-    console.log('listening on http://localhost:3001');
+    if (!(body.password)) {
+        return res.status(400).send({ error: "Data doesn't contain password or is formmated incorrectly" });
+    }
+
+    if (!(body.type)) {
+        return res.status(400).send({ error: "Data doesn't contain email or is formmated incorrectly" });
+    }
+
+    if (!(body.name)) {
+        return res.status(400).send({ error: "Data doesn't contain name or is formmated incorrectly" });
+    }
+
+    const user = new UserModel(body);
+    const salt = await bcrypt.genSalt(10);
+    
+    user.password = await bcrypt.hash(user.password, salt);
+    user.save().then((doc) => res.status(201).send(doc));
+});
+
+app.listen(PORT, () => {
+    console.log('listening on http://localhost:' + PORT);
 });
