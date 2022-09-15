58690a42637dd8a40e4fa5709b2dd08c476855dc
Owain <owain.evans@hotmail.co.uk>
2022-09-15 01:51:13 +0100

    Created login page for frontend

---
+++ frontend/src/components/Loginbox.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Loginbox.css";
+
+const Loginbox = () => {
+  return (
+    <div className="login-form">
+      <h2>Login</h2>
+      <form>
+        <div className="user-box">
+          <input type="text" name="" required=" "></input>
+          <label>Email</label>
+        </div>
+
+        <div className="user-box">
+          <input type="password" name="" required=" "></input>
+          <label>Username</label>
+        </div>
+
+        <div>
+          <a href="#" className="forgot-password">
+            Forgot password?
+          </a>
+        </div>
+
+        <a href="#" className="btn-submit">
+          Submit
+        </a>
+      </form>
+    </div>
+  );
+};
+
+export default Loginbox;
